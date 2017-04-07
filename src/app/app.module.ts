f91a8fd41e38462544c686ebe44906750bc66e9f
Bjorn S <bjorn@somnu.com>
2017-04-07 19:29:55 +0200

    Refactors menu into seperate component, adds base module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import {SharedModule} from './shared/shared.module'
-import {HttpClient} from './shared/http.client'
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
+import { AppComponent } from './app.component'
+import { RouterModule, Routes } from '@angular/router'
+import { SharedModule } from './shared/shared.module'
+import { HttpClient } from './shared/http.client'
+import { BaseModule } from './base/base.module'
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {HttpClient} from './shared/http.client'
     BrowserModule,
     FormsModule,
     HttpModule,
-    SharedModule
+    RouterModule,
+    SharedModule,
+    BaseModule    
   ],
   providers: [
     HttpClient
